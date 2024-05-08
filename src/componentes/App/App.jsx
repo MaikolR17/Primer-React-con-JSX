771c9bce0e467df8e6631e6f99c6bef3d032987a
Maikol Rodriguez <maikolrodriguezm@gmail.com>
2024-05-08 00:42:55 -0400

    Primer React con JSX

---
+++ src/componentes/App/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Lista from '../Lista/Lista';
+
+const texto = () => {
+  return (
+    <div>
+      <h1> Hola Dojo! </h1>
+      <h2> Cosas que necesito hacer: </h2>
+      <Lista />
+    </div>
+  );
+}
+
+export default texto;
---
+++ src/componentes/Lista/Lista.jsx
@@ -0,0 +1,14 @@
+const lista = () => {
+    return (
+        <div>
+            <ul>
+                <li> Aprender React. </li>
+                <li> Subir el Monte Everest. </li>
+                <li> Correr una maratón. </li>
+                <li> Alimentar al perro. </li>
+            </ul>
+        </div>
+    );
+}
+
+export default lista;
